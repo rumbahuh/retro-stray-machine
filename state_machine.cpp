46a273e46431a725c24606f610a1cb5c711a98e3
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2025-11-08 08:29:38 +0100

    Comentarios y autoría añadido. Ultrasonic interruption implemented with booleans.

--- state_machine.cpp
+++ state_machine.cpp
@@ -1,12 +1,23 @@
+/*
+Authoress: Rebeca Castilla
+Date: 7-8/Nov/2025
+Goal: Generar comportamiento de máquina expendedora
+      usando arduino y ciertos sensores y actuadores
+Notas: Añadiré comentarios para que los comentarios que añadiré
+       a futuro en el README.md.
+       Está mal tabulado sobre el estilo de C++, no tengo tests.
+*/
+
 #include <LiquidCrystal.h>
 
-// C++ code
-
-const int LED1 = 13;
-const int pin = 2;
+const int LED1 = 13; // Como en clase
+const int pin = 2; // Lectura - error de int0, no convierte, por qué
 const int ULTRASONIC_PIN = 9;
-volatile bool service_state = false;
+// Preguntar a profesor sobre qué pines usar mejor
 LiquidCrystal lcd(12, 11, 5, 4, 3, 7);
+
+volatile bool person_detected = false;
+
 enum State {IDLE, START, SERVICE};
 volatile State currentState = IDLE;
 
@@ -15,11 +26,23 @@ void start()
   currentState = START;
 }
 
+void person()
+{
+  person_detected = true;
+}
+
 void setup()
 {
   pinMode(LED1, OUTPUT);
+  // Por qué INPUT_PULLUP
+  // EN el momento en el que pulsas el botón
+  // read de pin, se activa la interrupción start()
   pinMode(pin, INPUT_PULLUP);
+  // Queremos leer del pin así que le pedimos INPUT de config
+  pinMode(ULTRASONIC_PIN, INPUT);
+  // Por qué FALLING
   attachInterrupt(digitalPinToInterrupt(pin), start, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ULTRASONIC_PIN), person, FALLING);
   lcd.begin(16, 2);
 }
 
@@ -31,18 +54,29 @@ void loop() {
             lcd.print("CARGANDO ...");
             for (int i=0; i<3; i++) {
                 digitalWrite(LED1, HIGH);
+                // Por qué delay() y no TimeOne
                 delay(1000);
                 digitalWrite(LED1, LOW);
                 delay(1000);
             }
+            // Aquí añadir watchdog reset
             currentState = SERVICE;
             break;
 
         case SERVICE:
-            lcd.clear();
+            /*lcd.clear();
             lcd.setCursor(0,0);
-            lcd.print("On service");
-            // service code
+            lcd.print("On service");*/
+            if (person_detected) {
+              lcd.clear();
+              lcd.setCursor(0,0);
+              lcd.print("DETECTED");
+            } else {
+              lcd.clear();
+              lcd.setCursor(0,0);
+              // La frase es muy larga jaja
+              lcd.print("ESPERANDO CLIENTE");
+            }
             currentState = IDLE;
             break;
     }
