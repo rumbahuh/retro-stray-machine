1394fb511f2555cc972d13a08069b41ccdc0f468
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2025-11-08 09:09:20 +0100

    He implementado la opción de cuando se aleje la persona vuelva a escribir esperando cliente, pero sé que dará errores a la hora de implementar admin options y el listado de cosas.. creo

---
+++ state_machine_u_no_iter_2.cpp
@@ -0,0 +1,88 @@
+/*
+Authoress: Rebeca Castilla
+Date: 7-8/Nov/2025
+Goal: Generar comportamiento de máquina expendedora
+      usando arduino y ciertos sensores y actuadores
+Notas: Añadiré comentarios para que los comentarios que añadiré
+       a futuro en el README.md.
+       Está mal tabulado sobre el estilo de C++, no tengo tests.
+*/
+
+#include <LiquidCrystal.h>
+
+const int LED1 = 13; // Como en clase
+const int pin = 2; // Lectura - error de int0, no convierte, por qué
+const int ULTRASONIC_PIN = 9;
+// Preguntar a profesor sobre qué pines usar mejor
+LiquidCrystal lcd(12, 11, 5, 4, 3, 7);
+
+volatile bool previously_detected_person = false;
+volatile bool first_iteration = true;
+
+enum State {IDLE, START, SERVICE};
+volatile State currentState = IDLE;
+
+void start()
+{
+  currentState = START;
+}
+
+void setup()
+{
+  pinMode(LED1, OUTPUT);
+  // Por qué INPUT_PULLUP
+  // EN el momento en el que pulsas el botón
+  // read de pin, se activa la interrupción start()
+  pinMode(pin, INPUT_PULLUP);
+  // Queremos leer del pin así que le pedimos INPUT de config
+  pinMode(ULTRASONIC_PIN, INPUT);
+  // Por qué FALLING
+  attachInterrupt(digitalPinToInterrupt(pin), start, FALLING);
+  /* No queremos usar interrupciones para el ultrasonic
+  porque no puedo asignar una interrupcion para cuando está
+  la persona a un metro y cuando no. Como tampoco puedo hacer
+  un digital read para no afectar a la velocidad del programa
+  considero que es mejor añadir un if de lectura del pin..
+  aunque me van a dar falsos positivos. Me quiere sonar que en clase
+  lo mencionamos.
+  */
+  lcd.begin(16, 2);
+}
+
+void loop() {
+    switch(currentState) {
+        case START:
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("CARGANDO ...");
+            for (int i=0; i<3; i++) {
+                digitalWrite(LED1, HIGH);
+                // Por qué delay() y no TimeOne
+                delay(1000);
+                digitalWrite(LED1, LOW);
+                delay(1000);
+            }
+            // Aquí añadir watchdog reset
+            currentState = SERVICE;
+            break;
+
+        case SERVICE:
+            bool person_now_detected = digitalRead(ULTRASONIC_PIN);
+
+            if (first_iteration || person_now_detected != previously_detected_person) {
+              lcd.clear();
+              lcd.setCursor(0,0);
+              
+              if (person_now_detected) {
+                lcd.print("DETECTED");
+              } else {
+                lcd.print("ESPERANDO CLIENTE");
+              }
+              
+              previously_detected_person = person_now_detected;
+              first_iteration = false;
+            }
+      
+            break;
+    }
+}
