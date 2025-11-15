534e36146ac7962c177e32b7e2f45950726aa515
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2025-11-15 08:49:39 +0100

    Código conjunto con sensores excepto joystick y de humedad. COmpila y funciona en la simulación, además he arreglado un bug del inicio.

---
+++ main.cpp
@@ -0,0 +1,155 @@
+/*
+Authoress: Rebeca Castilla
+Date: 7-8/Nov/2025
+Goal: Generar comportamiento de máquina expendedora
+      usando arduino y ciertos sensores y actuadores
+*/
+#include <LiquidCrystal.h>
+
+// -- Pines de interrupciones
+const int pin = 2;  // Botón
+// cont int pin = 3; // Botón de joystick
+// -- Pines de ledes
+const int LED1 = 11;
+// const int LED2 = 6;
+// -- Pines de pantalla lcd
+LiquidCrystal lcd(12, 13, 5, 4, 8, 7);
+// -- Pines de sensor de ultrasonidos
+const int PIN_TRIG = 9;
+const int PIN_ECHO = 10;
+// -- Pines de sensor de humedad
+// const int PIN_HUMEDAD = // ME FALTA un pin
+
+const double METER_LIGHT_SPEED_CONVERSION_FACTOR = 0.0001715;
+
+volatile bool previously_detected_person = false;
+volatile bool first_iteration = true;
+
+enum State { IDLE, START, SERVICE, ADMIN, OPTIONS };
+volatile State currentState = IDLE;
+volatile State previousState = IDLE;
+
+volatile unsigned long LOW_TO_HIGH_TIME = 0;
+volatile unsigned long HIGH_TO_LOW_TIME = 0;
+volatile bool button_ready = false;
+
+// LED blink variables
+unsigned long previousMillis = 0;
+const long blinkInterval = 500;  // 500 ms on/off
+bool ledState = LOW;
+
+void button_pressed() {
+  unsigned long now = millis();
+
+  if (digitalRead(pin) == HIGH) {
+    LOW_TO_HIGH_TIME = now;
+    button_ready = true;
+
+  } else {
+    HIGH_TO_LOW_TIME = now;
+  }
+
+  currentState = IDLE;
+}
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(pin, INPUT_PULLUP);
+  pinMode(PIN_ECHO, INPUT);
+  pinMode(PIN_TRIG, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(pin), button_pressed, CHANGE);
+  lcd.begin(16, 2);
+}
+
+double get_distance() {
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(PIN_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
+  double dis = pulseIn(PIN_ECHO, HIGH);
+  return dis * METER_LIGHT_SPEED_CONVERSION_FACTOR;
+}
+
+int getPulseTime() { return (LOW_TO_HIGH_TIME - HIGH_TO_LOW_TIME) / 1000.0; }
+
+void display(const char* message_to_display) {
+  // Uso const char* en lugar de String
+  // para no alocar dinámicamente
+  // ya que la RAM del microcontrolador Arduino
+  // es muy limitada
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(message_to_display);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  switch (currentState) {
+    case IDLE: {
+      if (button_ready) {
+        currentState = START;
+        button_ready = false;
+        previousState = IDLE;
+      }
+      break;
+    }
+    case START: {
+      // static unsigned long startTime = 0;
+      // if (startTime == 0) startTime = millis();  // initialize once
+
+      double pulseTime = getPulseTime();
+      if (pulseTime <= 2.9) {
+        if (previousState != currentState) {
+          display("CARGANDO");
+          previousState = currentState;
+        }
+      } else if (pulseTime > 2.9 && pulseTime < 4.0) {
+        currentState = ADMIN;
+        previousState = START;
+      } else {
+        currentState = OPTIONS;
+        previousState = START;
+      }
+      break;
+    }
+
+    case SERVICE: {
+      bool person_now_detected = false;
+
+      double distance = get_distance();
+      if (distance < 1) person_now_detected = true;
+
+      if (first_iteration ||
+          person_now_detected != previously_detected_person) {
+        if (previousState != currentState) {
+          if (person_now_detected)
+            display("DETECTED");
+          else
+            display("ESPERANDO CLIENTE");
+
+          previously_detected_person = person_now_detected;
+          first_iteration = false;
+        }
+        previousState = currentState;
+      }
+    } break;
+
+    case ADMIN:
+      if (previousState != currentState) {
+        display("ADMIN");
+        previousState = currentState;
+      }
+      break;
+
+    case OPTIONS:
+      if (previousState != currentState) {
+        display("OPTIONS");
+        previousState = currentState;
+      }
+      break;
+  }
+}
