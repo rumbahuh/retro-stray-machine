2a791d07e714a9a1192b0b234aa289d11a231959
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2025-11-20 18:24:14 +0100

    Quité la funcionalidad del botón porque ocmplicaba el código innecesariamente. Añadí la otra LED y conecté el joystick. El esquema lo que tengo que pasar a fritzing porque no hay joystick en tinkercad, he perdido un poco de tiempo, pero creo que llego.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <LiquidCrystal.h>
 
-// -- Pines de interrupciones
-const int pin = 2;  // Botón
-// cont int pin = 3; // Botón de joystick
+const int pin = 2;  // Interrupción -- Botón
+
+// -- Pines de Joystick
+const int joystick = 3; // Interrupción -- Botón de joystick
+const int Vx = A0;
+const int Vy = A1;
+
 // -- Pines de ledes
 const int LED1 = 11;
-// const int LED2 = 6;
+const int LED2 = 6;
+
 // -- Pines de pantalla lcd
 LiquidCrystal lcd(12, 13, 5, 4, 8, 7);
+
 // -- Pines de sensor de ultrasonidos
 const int PIN_TRIG = 9;
 const int PIN_ECHO = 10;
+
 // -- Pines de sensor de humedad
-// const int PIN_HUMEDAD = // ME FALTA un pin
+// const int PIN_HUMEDAD = A3 // Uso analógico porque no me quedan más pines
 
 const double METER_LIGHT_SPEED_CONVERSION_FACTOR = 0.0001715;
 
@@ -32,6 +39,36 @@ volatile unsigned long led_time = 0;
 volatile bool led_finished_non_blocking_delay = false;
 volatile int counter = 0;
 
+volatile bool exit_satisfied = false;
+volatile bool first_iteration_loop = true;
+
+volatile bool joystick_bool = false;
+
+volatile bool led_bool = true;
+
+int getPulseTime() { return (LOW_TO_HIGH_TIME - HIGH_TO_LOW_TIME) / 1000.0; }
+void button_unpressed() {
+  Serial.println("Button was released");
+  double pulseTime = getPulseTime();
+
+  if (pulseTime >= 2.0 && pulseTime <= 3.0) {
+    currentState = SERVICE;
+    first_iteration = true;
+    previousState = IDLE; // TENGO QUE cambiar logica para decir que en uno restart y el otro simplemente vuelve a service
+  } else if (pulseTime > 5) {
+    if (currentState == ADMIN) {
+      currentState = SERVICE;
+      previousState = ADMIN;
+      first_iteration = true; // Sin esto no vuelve a comprobar persona!
+    } else {
+      currentState = ADMIN;
+      previousState = SERVICE;
+    }
+  }
+
+  button_ready = false;
+}
+
 void button_pressed() {
   unsigned long now = millis();
 
@@ -42,17 +79,28 @@ void button_pressed() {
   } else {
     HIGH_TO_LOW_TIME = now;
   }
+}
 
+void joystick_pressed() {
+  joystick_bool = true;
   currentState = IDLE;
+
 }
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+
   pinMode(pin, INPUT_PULLUP);
+  pinMode(joystick, INPUT_PULLUP);
+
   pinMode(PIN_ECHO, INPUT);
   pinMode(PIN_TRIG, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(pin), button_pressed, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(joystick), joystick_pressed, FALLING);
+
   lcd.begin(16, 2);
 }
 
@@ -66,8 +114,6 @@ double get_distance() {
   return dis * METER_LIGHT_SPEED_CONVERSION_FACTOR;
 }
 
-int getPulseTime() { return (LOW_TO_HIGH_TIME - HIGH_TO_LOW_TIME) / 1000.0; }
-
 void display(const char* message_to_display) {
   // Uso const char* en lugar de String
   // para no alocar dinámicamente
@@ -80,66 +126,53 @@ void display(const char* message_to_display) {
 }
 
 void loop() {
-  switch (currentState) {
-    case IDLE: {
-      if (button_ready) {
-        currentState = START;
-        button_ready = false;
-        previousState = IDLE;
-      }
-      break;
+  if (led_bool) {
+    if (led_start == 0) {
+        display("CARGANDO");
+        digitalWrite(LED1, HIGH);
+        led_start = millis();
+        Serial.println("led started");
     }
-    case START: {
-      unsigned long millisStart = millis();
 
-      double pulseTime = getPulseTime();
-      if (pulseTime <= 2.9) {
-        if (previousState != currentState) {
-          display("CARGANDO");
-          counter = 0;
-          digitalWrite(LED1, HIGH);
-          led_start = millis();
-          previousState = currentState;
-        }
+    unsigned long now = millis();
+    unsigned long elapsed = now - led_start;
 
-        if (led_start > 0) {
-          unsigned long now = millis();
-          unsigned long elapsed = now - led_start;
+    if (elapsed >= 1000) {
+      digitalWrite(LED1, !digitalRead(LED1));
+      led_start = now;
 
-          if (elapsed >= 1000) {                     // toggle every 1 second
-            digitalWrite(LED1, !digitalRead(LED1));  // toggle LED
-            led_start = now;                         // reset timer
-
-            if (digitalRead(LED1) == LOW)
-              counter++;  // count full ON->OFF cycles
-          }
-
-          if (counter >= 3) {         // after 3 full ON->OFF cycles
-            digitalWrite(LED1, LOW);  // ensure LED is off
-            counter = 0;
-            led_start = 0;
-            currentState = SERVICE;
-            previousState = START;
-          }
-        }
-      } else if (pulseTime > 2.9 && pulseTime < 4.0) {
-        currentState = ADMIN;
-        previousState = START;
+      if (digitalRead(LED1) == LOW) {
+        counter++;
       }
-      else {
-        currentState = OPTIONS;
+
+      if (counter >= 3) {
+        digitalWrite(LED1, LOW);
+        counter = 0;
+        led_start = 0;
+        led_bool = false;
         previousState = START;
+        currentState = SERVICE;
       }
-      break;
+    }
+  }
+  if (button_ready) {
+    button_unpressed();
+  }
+  
+  if(joystick_bool) {
+    Serial.println("Joystick was pressed!!");
+    joystick_bool = false;
   }
 
-  case SERVICE: {
-    bool person_now_detected = false;
+  switch (currentState) {
+    case SERVICE: {
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      bool person_now_detected = false;
+      double distance = get_distance();
+      unsigned long millisStart = millis();
 
-    double distance = get_distance();
-    if (distance < 1) person_now_detected = true;
-
-    if (first_iteration || person_now_detected != previously_detected_person) {
+      if (first_iteration || person_now_detected != previously_detected_person) {
       if (previousState != currentState) {
         if (person_now_detected)
           display("DETECTED");
@@ -151,20 +184,19 @@ void loop() {
       }
       previousState = currentState;
     }
-  } break;
+      first_iteration_loop = false;
+      break;
+    }
 
-  case ADMIN:
+  case ADMIN: {
     if (previousState != currentState) {
       display("ADMIN");
       previousState = currentState;
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
     }
     break;
+    }
+  }
+}
 
-  case OPTIONS:
-    if (previousState != currentState) {
-      display("OPTIONS");
-      previousState = currentState;
-    }
-    break;
-}
-}
